5435e67b1fbe6da0f5302644d0bf285bf844cb7e
Daniela <danielacamayo@yahoo.es>
2025-03-05 15:42:05 -0500

    cambio en el mensaje hello

--- src/main/java/com/example/my_java_api/controller/MyController.java
+++ src/main/java/com/example/my_java_api/controller/MyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, Docker!";
     }
 
 }
